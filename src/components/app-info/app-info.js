28c65bb1d0155442fe3e47014b3ccacc8a23c2d7
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-03-12 22:54:58 -0400

    reorganization of files and folers

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Accounting of employees in company.</h1>
+            <h2>Total quantity of employees: </h2>
+            <h2>The award will be given to: </h2>
+        </div>
+    )
+} 
+
+export default AppInfo;
