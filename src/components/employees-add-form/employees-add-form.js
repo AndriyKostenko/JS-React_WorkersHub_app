49962ce365f5848dc2a0e0be636db29b3161212a
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-03-18 20:50:09 -0400

    added name & salary validation

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import FormErrors from '../form-errors/form-errors';
+
 import './employees-add-form.css'
 
 
@@ -8,24 +10,63 @@ class EmployeesAddForm extends Component {
         super(props)
         this.state = {
             name: '',
-            salary: ''
+            salary: '',
+            formErrors: {name: '', salary:''},
+            nameValid: false,
+            salaryValid: false,
+            formValid: false
         }
     }
 
     // event.target takes automatically through DOM
     onValueChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({[name]: value}, () => {this.validateField(name, value)})   
     }
+
+
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let nameValid = this.state.nameValid;
+        let salaryValid = this.state.salaryValid;
+
+        switch(fieldName) {
+            case 'name':
+                nameValid = value.length >= 2;
+                fieldValidationErrors.name = nameValid ? '': ' is too short';
+                break;
+            case 'salary':
+                salaryValid = !isNaN(value) && 
+                                typeof(parseInt(value)) === 'number' && 
+                                !isNaN(parseInt(value, 10));
+                fieldValidationErrors.salary = salaryValid ? '': ' is not a number'
+                break;
+            default:
+                break;
+            } 
+
+        this.setState({formErrors: fieldValidationErrors,
+                        nameValid: nameValid,
+                        salaryValid:salaryValid},
+                        this.validateForm)
+
+    }
+
+
+    validateForm() {
+        this.setState({formValid: this.state.nameValid && this.state.salaryValid});
+      }
+
+
 
     onSubmit = (event) => {
         event.preventDefault();
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
             name:'',
-            salary:''
-        })
+            salary:''})
     }
 
     // adding an event listeners: onSubmit, onChange to fomr and input 
@@ -38,26 +79,33 @@ class EmployeesAddForm extends Component {
                 <form 
                     className="add-form d-flex"
                     onSubmit={this.onSubmit}>
+                        <FormErrors formErrors={this.state.formErrors}/>
                     <input type="text"
                             className="form-control new-post-label"
                             placeholder="What's the name?"
                             name="name"
                             value={name}
-                            onChange={this.onValueChange}/>
+                            onChange={(event) => this.onValueChange(event)}/>
                     <input type="number"
                             className="form-control new-post-label"
                             placeholder="Wages in $ ?"
                             name="salary"
                             value={salary}
-                            onChange={this.onValueChange}/>
+                            onChange={(event) => this.onValueChange(event)}/>
     
                     <button type="submit"
-                            className="btn btn-outline-light">Add</button>
+                            className="btn btn-outline-light"
+                            disabled={!this.state.formValid}>Add</button>
+                            
+                    
                 </form>
             </div>
         )
     } 
 
 }
+
+
+
 
 export default EmployeesAddForm;
